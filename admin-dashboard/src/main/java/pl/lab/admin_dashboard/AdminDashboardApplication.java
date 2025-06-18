a3950c1aed7276b32559a61a37abd7d1b976301b
Bartosz Budzynowski <bartoszbud@gmail.com>
2025-06-18 23:15:00 +0000

    mods and fixes

--- admin-dashboard/src/main/java/pl/lab/admin_dashboard/AdminDashboardApplication.java
+++ admin-dashboard/src/main/java/pl/lab/admin_dashboard/AdminDashboardApplication.java
@@ -2,8 +2,11 @@ package pl.lab.admin_dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @EnableAdminServer
 public class AdminDashboardApplication {
 
